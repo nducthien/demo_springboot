24da12379c0d3fa6c26352232283b845d84bad36
thiennd <thiennd@nal.vn>
2020-03-17 18:29:20 +0700

    demo new api

--- src/main/java/com/example/demo/controller/NewController.java
+++ src/main/java/com/example/demo/controller/NewController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.New;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@EnableAutoConfiguration
 public class NewController {
-	@RequestMapping(value = "/new", method = RequestMethod.POST)
 
-	@ResponseBody
+	@PostMapping(value = "/new")
 	public New createNew(@RequestBody New model) {
+		return model;
+	}
+
+	@PostMapping(value = "/new")
+	public New updateNew(@RequestBody New model) {
 		return model;
 	}
 }
